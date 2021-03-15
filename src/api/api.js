629f07bd036c0859f7d86692211d502497ee72db
reboot-ua <reboot.if.ua@gmail.com>
2021-03-15 08:13:09 +0200

    85 hook

--- src/api/api.js
+++ src/api/api.js
@@ -5,7 +5,7 @@ const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers:     {
-        "API-KEY": "b1775b2f-c3a5-4509-8dc9-90b5629de7c3"
+        "API-KEY": "4957933a-8a61-407d-a87e-72b4e872954b"
     }
 });
 
--- src/hoc/withAuthRedirect.js
+++ src/hoc/withAuthRedirect.js
@@ -1,6 +1,6 @@
 import React from "react";
-import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
+import {Redirect} from "react-router-dom";
 
 let mapStateToPropsForRedirect = (state) => ({
     isAuth: state.auth.isAuth
@@ -10,10 +10,10 @@ export const withAuthRedirect = (Component) => {
 
     class RedirectComponent extends React.Component {
         render() {
-            if (!this.props.isAuth) return <Redirect to='/login' />;
+            if (!this.props.isAuth) return <Redirect to='/login' />
 
             return <Component {...this.props}/>
-         }
+        }
     }
 
     let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
