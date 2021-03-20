19cbf1cc46ceb77a37f799ec24aec80b9a3d5537
reboot-ua <reboot.if.ua@gmail.com>
2021-03-20 16:41:58 +0200

    Updates

---
+++ precache-manifest.c8fcab5d84452aac9fee1926d33b42de.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbfbc77cb37f1050e258e444c2326e65",
+    "url": "/demo-network/index.html"
+  },
+  {
+    "revision": "894e21680c2a8678f9e3",
+    "url": "/demo-network/static/css/3.ee2b2192.chunk.css"
+  },
+  {
+    "revision": "ad1d29dda39bf5d0c920",
+    "url": "/demo-network/static/css/4.ed6641e1.chunk.css"
+  },
+  {
+    "revision": "2f5ab632e46082af48f0",
+    "url": "/demo-network/static/css/main.3e040afe.chunk.css"
+  },
+  {
+    "revision": "3c228a5cb50a65fcb584",
+    "url": "/demo-network/static/js/2.83db7e46.chunk.js"
+  },
+  {
+    "revision": "894e21680c2a8678f9e3",
+    "url": "/demo-network/static/js/3.adff7ca1.chunk.js"
+  },
+  {
+    "revision": "ad1d29dda39bf5d0c920",
+    "url": "/demo-network/static/js/4.2fad730f.chunk.js"
+  },
+  {
+    "revision": "2f5ab632e46082af48f0",
+    "url": "/demo-network/static/js/main.172de924.chunk.js"
+  },
+  {
+    "revision": "5d8056d5d1205f0cfcac",
+    "url": "/demo-network/static/js/runtime~main.80935483.js"
+  },
+  {
+    "revision": "6e7c9db7e1f100edac7c6e48714d2ea0",
+    "url": "/demo-network/static/media/preloader.6e7c9db7.svg"
+  },
+  {
+    "revision": "8b1dbb1f4e4468b9cc31ced9c63fcfe7",
+    "url": "/demo-network/static/media/user.8b1dbb1f.png"
+  }
+]);
